75412f47f1171bd483880715a26d9edb406443cc
Mekhak <mexakshoyan@gmail.com>
2021-09-11 09:23:53 -0700

    hw-01

---
+++ hw-01/README.md
@@ -0,0 +1,4 @@
+# Files via cout
+
+Write something into a file using the cout object, open and close functions and nothing else.
+
---
+++ hw-01/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    const char* fname = "something.txt";
+
+    std::cout << "Writing something in "
+              << fname
+              << " via std::cout ..."
+              << std::endl;
+
+    // close stdout
+    close(1);
+
+    int fd = open("something.txt", O_CREAT | O_WRONLY, S_IRWXU);
+
+    if (fd == -1) {
+        return errno;
+    }
+
+    std::cout << "some text ..." << std::endl;
+
+    if (close(fd) == -1) {
+        return errno;
+    }
+
+    return 0;
+}
